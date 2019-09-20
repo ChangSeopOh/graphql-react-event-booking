b40eb3b7c0322cfcae14f932cf69e60b746ab6dd
Changseop OH <jaden.oh@yahoo.com>
2019-09-19 21:49:44 -0700

    added a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,33 +3,32 @@ import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
+
 import './App.css';
 
 function App() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Redirect from="/" to="/auth" exact/>
-                <Route path="/auth" component={AuthPage}/>
-                <Route path="/events" component={EventsPage}/>
-                <Route path="/bookings" component={BookingsPage}/>
-            </Switch>
+            <React.Fragment>
+                <MainNavigation/>
+                <main className="main-content">
+                    <Switch>
+                        <Redirect from="/" to="/auth" exact="exact"/>
+                        <Route path="/auth" component={AuthPage}/>
+                        <Route path="/events" component={EventsPage}/>
+                        <Route path="/bookings" component={BookingsPage}/>
+                    </Switch>
+                </main>
+            </React.Fragment>
         </BrowserRouter>
     );
 }
 
-
-// class App extends Component {
-//     render() {
-//         return (
-//             <BrowserRouter>
-//                 <Route path="/" component={null}/>
-//                 <Route path="/auth" component={null}/>
-//                 <Route path="/event" component={null}/>
-//                 <Route path="/booking" component={null}/>
-//             </BrowserRouter>
-//         );
-//     }
-// }
+// class App extends Component {     render() {         return (
+// <BrowserRouter>                 <Route path="/" component={null}/>
+// <Route path="/auth" component={null}/>                 <Route path="/event"
+// component={null}/>                 <Route path="/booking" component={null}/>
+// </BrowserRouter>         );     } }
 
 export default App;
